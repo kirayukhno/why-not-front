3ef9b928a4391834ccdc5a57eb43eff24a73966a
Maks Shkafar <m.shkafar@gmail.com>
2026-04-03 20:55:54 +0200

    Configure axios with NEXT_PUBLIC_API_URL for backend connection

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export const nextServer = axios.create({
   baseURL,
-  withCredentials: false,
+  withCredentials: true, // важно для куки
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
