20c1660fae68112709f2f2f9585f8c8aafce7915
Maks Shkafar <m.shkafar@gmail.com>
2026-04-05 20:07:41 +0200

    delete api request

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 export const nextServer = axios.create({
   baseURL: 'http://localhost:3000/api',
-  withCredentials: true, // дозволяє axios працювати з cookie
+  withCredentials: true,
 });
